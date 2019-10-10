8b39760da47259ff33aba980ccadb54049e06408
AlexFox1777 <mix.filipov@mail.ru>
2019-10-10 11:19:10 -0700

    server is able to get xml

--- api/server.js
+++ api/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const server = express();
 const usersRouter = require('../routers/users');
+const xmlparser = require("express-xml-bodyparser");
 
 require('dotenv').config();
 const helmet = require('helmet');
 const cors = require('cors');
 
 server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(xmlparser());
 server.use(helmet());
 server.use(cors());
 
