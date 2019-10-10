86b0ba322ffc5e691cfc6048437e3210ce1b5668
AlexFox1777 <mix.filipov@mail.ru>
2019-10-10 16:34:29 -0700

    destructure xform data

--- routers/users.js
+++ routers/users.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 const db = require('./usersDB');
-const xform2json = require('xform-to-json');
-const FormSubmissionMiddleware = require('openrosa-form-submission-middleware');
 
 
 router.post('/', (req, res) => {
     const user = req.body;
+    console.log('body ', req.body);
     console.log('hello heroku ', req.body);
     if (user) {
         db.add(user)
@@ -14,42 +13,20 @@ router.post('/', (req, res) => {
     }
 });
 
-router
-    .post('/odk/', convertForm, (req, res) => {
-    const user = req.body;
-
-    console.log("req.submission ", req.submission);
-    console.log("req.submission.json ", req.submission.json);
-    res.status(200).json(req.body);
-    /* if (user) {
-         db.add(user)
-             .then(([user]) => res.status(201).json(user))
-             .catch(err => res.status(500).json({error: "There was an error while saving the user to the database"}))
-     }*/
+router.post('/odk/', (req, res) => {
+    const {"h:html" : root} = req.body;
+    const {"h:head": head } = root;
+    const headData = head[0];
+    const {"model" : model} = headData;
+    const modelData = model[0];
+    const {"instance" : instance} = modelData;
+    const instanceData = instance[0];
+    const {"data": data} = instanceData;
+    const {"username" : username} = data[0];
+    const {"password" : password} = data[0];
+    console.log("username ", username, "password ", password);
+    res.status(200).json("hello");
 });
 
-
-function convertForm(req, res, next) {
-    console.log('REQ.BODY FROM CONVERTER ', req.body);
-
-    const userId = req.query.id;
-    const id = req.params.id;
-    const meta = {
-        id: id,
-        userId: userId,
-    };
-    console.log('QUERT.ID ', userId);
-    console.log('ID ', id);
-
-    xform2json(req.body, meta, function (err, form) {
-        console.log('FORM ', form);
-            req.submission = {
-                json: form,
-                xml: req.body,
-                metaData: meta.userId
-            };
-            next();
-    });
-}
 
 module.exports = router;
