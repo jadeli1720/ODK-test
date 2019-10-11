e4fce93bf81ee30a71f9e8fe2d487776deb0ef2b
AlexFox1777 <mix.filipov@mail.ru>
2019-10-11 08:59:56 -0700

    Saving, reading, parsing to json

--- api/server.js
+++ api/server.js
@@ -2,20 +2,20 @@ const express = require('express');
 const server = express();
 const usersRouter = require('../routers/users');
 const xmlparser = require("express-xml-bodyparser");
-const fileupload = require('express-fileupload');
+// const fileupload = require('express-fileupload');
 
 require('dotenv').config();
 const helmet = require('helmet');
 const cors = require('cors');
 
-server.use(express.urlencoded({ extended: true }));
+// server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 server.use(xmlparser());
 server.use(helmet());
 server.use(cors());
-server.use(fileupload({
+/*server.use(fileupload({
     useTempFiles: true,
-}));
+}));*/
 
 server.use('/users', usersRouter);
 server.get('/', (req, res) => {
