6a53dcdf163017ffe142649cb87279d30abe297c
AlexFox1777 <mix.filipov@mail.ru>
2019-10-10 19:26:12 -0700

    check the data from file

--- routers/users.js
+++ routers/users.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const db = require('./usersDB');
+const fs = require('fs');
+const xml2js = require('xml2js');
+const parser = new xml2js.Parser();
 
 
 router.post('/', (req, res) => {
@@ -27,23 +30,14 @@ router.post('/odk/', (req, res) => {
     console.log('username ', username, 'password ', password);*/
     console.log('BODY ', req.body);
     console.log('FILE ', req.files);
+    fs.readFile(req.files, (err, data) => {
+        if (err) throw err;
+        console.log('Data XML ', data);
+        parser.parseString(data, function (err, result) {
+            console.log('FROM XML TO JSON ', result);
+        })
+    });
     res.status(200).json("hello");
 });
-
-router.post('/odk/formUpload', (req, res) => {
-    const {"h:html" : root} = req.body;
-    const {"h:head": head } = root;
-    const headData = head[0];
-    const {"model" : model} = headData;
-    const modelData = model[0];
-    const {"instance" : instance} = modelData;
-    const instanceData = instance[0];
-    const {"data": data} = instanceData;
-    const {"username" : username} = data[0];
-    const {"password" : password} = data[0];
-    console.log("username ", username, "password ", password);
-    res.status(200).json("hello");
-});
-
 
 module.exports = router;
